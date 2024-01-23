aa91720b01a55f078c460fe786dc65cd549a4f23
Sami Munir <smunir01@outlook.com>
2024-01-23 01:12:33 -0500

    adding styles.css + font

--- React Counter App/react-counter-app/src/index.js
+++ React Counter App/react-counter-app/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './components/app/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>React Counter App</h1>
+    <App/>
   </React.StrictMode>
 );
