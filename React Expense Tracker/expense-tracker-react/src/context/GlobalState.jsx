ab9aaac8dc8085f59c1c279d48b381b877759c5e
Sami Munir <smunir01@outlook.com>
2024-01-24 15:42:42 -0500

    implementing DELETE_TRANSACTION action in Transaction component

--- React Expense Tracker/expense-tracker-react/src/context/GlobalState.jsx
+++ React Expense Tracker/expense-tracker-react/src/context/GlobalState.jsx
@@ -27,7 +27,7 @@ export const GlobalProvider = ({children}) => {
     }
     
     return (
-        <GlobalContext.Provider value={{transactions: state.transactions}}>
+        <GlobalContext.Provider value={{transactions: state.transactions, deleteTransaction}}>
             {children}
         </GlobalContext.Provider>
     );
