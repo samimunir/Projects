f51e269f449fc3e1fa7ef49ca741c0a639eb7deb
Sami Munir <smunir01@outlook.com>
2024-01-24 03:59:24 -0500

    updating IncomeExpenses component

--- React Expense Tracker/expense-tracker-react/src/components/IncomeExpenses.jsx
+++ React Expense Tracker/expense-tracker-react/src/components/IncomeExpenses.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 
 const IncomeExpenses = () => {
   return (
-    <div>IncomeExpenses</div>
+    <div className='inc-exp-container'>
+        <div>
+            <h4>Income</h4>
+            <p id='money-plus' class='money plus'>+$0.00</p>
+        </div>
+        <div>
+            <h4>Expenses</h4>
+            <p id='money-minus' class='money minus'>-$0.00</p>
+        </div>
+    </div>
   );
 }
 
